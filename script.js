da01d130d2d7f35ab7e91d5a0c38fa76203f82fe
nunju9a <51241293+nunju9a@users.noreply.github.com>
2019-06-13 11:41:04 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,48 +2,35 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-const listOfStudents = document.getElementsByClassName('student-item cf');
+//Adding global variables for the student list and the number of students per page to show
+
+let listOfStudents = document.querySelectorAll('.student-item');
 //console.log(listOfStudents);
-
 const itemsPerPage = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+// Function to only show 10 students at a time, hiding the rest of the student list
+
+const showPage = (list, page) => {
+   let indexStart = (page * itemsPerPage) - itemsPerPage;          // Points to the first student based on their index number
+   let indexEnd = page * itemsPerPage;                            // Points to the last student based on their index number
+
+   for (let x = 0; x < list.length; x++) {                        // Looping through student list
+      if (x >= indexStart && x <indexEnd) {
+         list[x].style.display = " ";                            // Displays student list between indexStart and indexEnd
+      } else {
+         list[x].style.display = "none";                         // Hides all other students
+      }
+   }
+}
+//showPage(listOfStudents, 5);
 
 
+// Function to create, append, and show pagination links
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
+const 
 
 
 
