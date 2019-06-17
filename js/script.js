3cd534ad2b6f3ee2af6c534a87a7c0937ecdc983
nunju9a <51241293+nunju9a@users.noreply.github.com>
2019-06-16 22:40:50 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -25,18 +25,18 @@ const showPage = (list, page) => {
 
 // Function to create, append, and show pagination links
 const appendPageLinks = (list) => {
-   let totalPages = Math.ceil(list.length/itemsPerPage);                       // Calculates total number of pages needed
+   let totalPages = Math.ceil(list.length/itemsPerPage);                     // Calculates total number of pages needed
 
    // Creating div and ul for links
-   const div = document.createElement('div');                               // Creating div
-   div.className = 'pagination';                                           // Giving class of pagination
-   document.querySelector('.page').appendChild(div);                      // Appending to .page div
-   const ul = document.createElement ('ul');                             // Creating ul
-   div.appendChild (ul);                                                // Appending to pagination div
+   const div = document.createElement('div');                              // Creating div
+   div.className = 'pagination';                                          // Giving class of pagination
+   document.querySelector('.page').appendChild(div);                     // Appending to .page div
+   const ul = document.createElement ('ul');                            // Creating ul
+   div.appendChild (ul);                                               // Appending to pagination div
 
    // Looping through to add <li> and <a> tags with page number text
    for (let x = 0; x < totalPages; x++) {
-      let li = document.createElement("li");                        // Creating <li> tag       
+      let li = document.createElement("li");                       // Creating <li> tag       
       let a = document.createElement("a");                         // Creating <a> tag
       a.textContent = `${x + 1}`;                                 // Creating page numbers
       a.href = `#`;                                              // Creating link
@@ -53,60 +53,59 @@ const appendPageLinks = (list) => {
       const link = e.target;
       let pageNumber;
       if (link.tagName === 'A') {
-         pageNumber = e.target.textContent;                              // Get page number from pressed link    
+         pageNumber = e.target.textContent;                           // Get page number from pressed link    
          const links = document.querySelectorAll ('.pagination a');
          for (let x = 0; x < links.length; x++) {
-            links [x].classList.remove ('active');                    // Removes active class from all other links
+            links [x].classList.remove ('active');                 // Removes active class from all other links
          }
-         link.className = "active";                                 // Makes current link active   
+         link.className = "active";                              // Makes current link active   
       } 
-      showPage (list,pageNumber);                                 // Calling showPage function everytime page link is clicked
+      showPage (list,pageNumber);                              // Calling showPage function everytime page link is clicked
    });
 }
 
 
-showPage (listOfStudents,1);                                   // Calls first page of students when page originally loads
-appendPageLinks (listOfStudents);                             // Calling appendPageLinks function
-
-
+showPage (listOfStudents,1);                             // Calls first page of students when page originally loads
+appendPageLinks (listOfStudents);                       // Calling appendPageLinks function
 
 
 // ATTEMPTING EXTRA CREDIT BELOW
 
 
 // Adding a search bar with input field
-const searchStudents = document.createElement ('div');                                             // Creating div for search            
-searchStudents.className = "student-search";                                                      // Giving Class Name
+const searchStudents = document.createElement ('div');                      // Creating div for search            
+searchStudents.className = "student-search";                               // Giving Class Name
 const pageHeader = document.querySelector ('.page-header');
-pageHeader.appendChild(searchStudents);                                                         // Appending div to page header
-const searchButton = document.createElement ('button');                                        // Creating button element
-searchStudents.appendChild (searchButton);                                                    // Appending button to search 
-searchButton.textContent = `Search`;                                                         // Labeling button as 'Search'
-const inputText = document.createElement ('input');                                         // Creating input element for text box
-searchStudents.appendChild (inputText);                                                    // Appending text box to search
-inputText.setAttribute ('placeholder', "Search for students");                            // Puttinng "Search for students" as placeholder
-const noResults = document.createElement ('p');                                          // Creating p tag for potential "No results" message
-searchStudents.appendChild(noResults);                                                  // Appending noResults to search div
-noResults.textContent = '';                                                            // Storing empty string as value in noResults
-let searchResults = [];                                                               // Creating empty array for search results
+pageHeader.appendChild(searchStudents);                                  // Appending div to page header
+const searchButton = document.createElement ('button');                 // Creating button element
+searchStudents.appendChild (searchButton);                             // Appending button to search 
+searchButton.textContent = `Search`;                                  // Labeling button as 'Search'
+const inputText = document.createElement ('input');                  // Creating input element for text box
+searchStudents.appendChild (inputText);                             // Appending text box to search
+inputText.setAttribute ('placeholder', "Search for students");     // Puttinng "Search for students" as placeholder
+const noResults = document.createElement ('p');                   // Creating p tag for potential "No results" message
+searchStudents.appendChild(noResults);                           // Appending noResults to search div
+noResults.textContent = '';                                     // Storing empty string as value in noResults
+let searchResults = [];                                        // Creating empty array for search results
 
 // Search Function that compares the input of the search bar with the student list, then displays results
 const searchInput = () => {
+   
    // Looping through student list
    for (let x = 0; x < listOfStudents.length; x++) {
       //Conditional statement to test the value of each student and see if it includes the search input value                                      
       if (listOfStudents[x].textContent.toLowerCase().includes(inputText.value.toLowerCase())) {                        
-         listOfStudents[x].style.display = "block";                          // Displays student content if a match
-         searchResults.push(listOfStudents[x]);                             // Stores each student in the searchResults array
+         listOfStudents[x].style.display = "block";                 // Displays student content if a match
+         searchResults.push(listOfStudents[x]);                    // Stores each student in the searchResults array
       } else {
-         listOfStudents[x].style.display = "none";                        // Hides all students who do not match any input value
+         listOfStudents[x].style.display = "none";               // Hides all students who do not match any input value
         }
    }
 }
 
 // Adding 'click' eventListener for search button which calls the search function and displays results 
 searchButton.addEventListener ('click', (e) => {
-  searchInput();
+   searchInput();
   const page = document.querySelector('.page');
   const pagination = document.querySelector('.pagination');
   page.removeChild(pagination);
@@ -130,4 +129,5 @@ searchButton.addEventListener ('keyup', (e) => {
  }
  showPage(searchResults, 1);                                  // Calls showPage function with searchResults as argument
  appendPageLinks(searchResults);                             // Calls appendPageLinks function with searcResults as argument
+
  } ) ;
